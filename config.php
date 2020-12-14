9e721be61b0b3f4cbbb1eba244d0b51f06b1a4c8
Jhansidunuka <55626958+Jhansidunuka@users.noreply.github.com>
2020-12-14 11:40:38 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 
 <?php
-class mydb extends SQLite3 {
+class mydatabase extends SQLite3 {
       function __construct() {
          $this->open('mydatabase.db');
       }
 }
-$db = new mydb();
+$db = new mydatabase();
 
 
 $result = $db->exec('CREATE TABLE IF NOT EXISTS List(IP varchar not null, VLANs varchar not null, PORT varchar, MACS varchar)');
 if(!$result){
-   echo $db->ErrorMsg(); 
+   echo $db->LastErrorMsg(); 
 }
 else {
    pass;
@@ -18,7 +18,7 @@ else {
 
 $result = $db->exec('CREATE TABLE IF NOT EXISTS info(IP varchar not null,PORT varchar not null,COMMUNITY string not null ,VERSION varchar not null, FIRST_PROBE varchar, LATEST_PROBE varchar null, FAILED_ATTEMPTS int default 0 not null)');
    if(!$result){
-      echo $db->ErrorMsg();
+      echo $db->LastErrorMsg();
    }
    else {
       pass;
